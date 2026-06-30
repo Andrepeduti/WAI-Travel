f236be6215735de56c22de745d7b030749e1e602
Andrepeduti <andre.peduti@hotmail.com>
2026-06-30 16:22:25 -0300

    feat: ajuste autenticação

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -13,7 +13,9 @@ const config: CapacitorConfig = {
   plugins: {
     GoogleAuth: {
       scopes: ['profile', 'email'],
-      serverClientId: '617657830623-57hvjngsel4rl20gef74ek9csrrasau6.apps.googleusercontent.com', // Preencher depois
+      serverClientId: '617657830623-57hvjngsel4rl20gef74ek9csrrasau6.apps.googleusercontent.com',
+      iosClientId: '617657830623-ev1u0brv0e40utkhv668u4oeqataoel3.apps.googleusercontent.com',
+      androidClientId: '617657830623-g19lo73is8k7p6bp7crmr2bd0h5te714.apps.googleusercontent.com',
       forceCodeForRefreshToken: true,
     },
   }
